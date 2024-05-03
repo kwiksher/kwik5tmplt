c437fe200ca80bc4c1af3fd4dba52e7cf528b371
Naoya Yamamoto <yamamoto@kwiksher.com>
2024-05-03 12:06:18 +0900

    Add template layer_image.lua

--- Solar2D/App/bookX/main.lua
+++
@@ -1,1 +0,0 @@
-require("controller.index").bootstrap({name="{{book}}", sceneIndex = {{sceneIndex}}, position = {x=0, y=0}}) -- scenes.index
--- Solar2D/editor/template/main.lua
+++ Solar2D/editor/template/main.lua
@@ -1,1 +1,21 @@
-require("controller.index").bootstrap({name="{{book}}", sceneIndex = {{sceneIndex}}, position = {x=0, y=0}}) -- scenes.index
+--require("controller.index").bootstrap({name="{{book}}", sceneIndex = {{sceneIndex}}, position = {x=0, y=0}}) -- scenes.index
+
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+  local lldebugger = loadfile(os.getenv("LOCAL_LUA_DEBUGGER_FILEPATH"))()
+  lldebugger.start()
+end
+
+inspect = require("extlib.inspect")
+
+local common = {
+  commands = {"myEvent"},
+  components = {
+    -- "align",
+    "myComponent",
+    "thumbnailNavigation",
+    "index" -- this loads editor!
+  }
+}
+
+require("controller.index").bootstrap({name="{{book}}", sceneIndex = {{sceneIndex}}, position = {x=0, y=0}, common = common}) -- scenes.index
+
