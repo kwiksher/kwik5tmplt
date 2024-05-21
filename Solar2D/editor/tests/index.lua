0f0dc2ef23d3427a711b35e1d79e9766d77dcde5
Naoya Yamamoto <nyamamoto@neoscorp.jp>
2024-05-21 09:46:24 +0900

    FIX mui icon size and position

--- Solar2D/editor/tests/index.lua
+++ Solar2D/editor/tests/index.lua
@@ -4,7 +4,7 @@ local M = {
     print("============ lunatest =============")
     -- lunatest.suite("editor.tests.suite_page1_animation", props)
     --lunatest.suite("editor.tests.suite_assets", props)
-    --lunatest.suite("editor.tests.suite_page1_action", props)
+    lunatest.suite("editor.tests.suite_page1_action", props)
     -- lunatest.suite("editor.tests.suite_misc", props)
     --lunatest.suite("editor.tests.suite_new_shape_transform", props)
     -- lunatest.suite("editor.tests.suite_new_book_page_layer", props)
