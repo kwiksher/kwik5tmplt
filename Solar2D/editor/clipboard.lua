e141def467d92ce8397636d426c57ee0c0225ae6
Naoya Yamamoto <yamamoto@kwiksher.com>
2024-07-25 11:25:53 +0900

    FIX copy & paste an action

--- Solar2D/editor/clipboard.lua
+++ Solar2D/editor/clipboard.lua
@@ -1,9 +1,29 @@
 local M = {}
-
-function M:save()
+local json = require("json")
+local path = system.pathForFile("clipboard.json", system.TemporaryDirectory)
+--
+function M:save(data)
+  --
+  local file, errorString = io.open(path, "w")
+  if not file then
+    print("ERROR: " .. errorString)
+  else
+    local contents = json.encode(data)
+    file:write(contents)
+    io.close(file)
+  end
 end
 
 function M:read()
+  local contents = "{}"
+  local file, errorString = io.open(path, "r")
+  if not file then
+    print("ERROR: " .. errorString)
+  else
+    contents = file:read()
+    io.close(file)
+  end
+  return json.decode(contents)
 end
 
 return M
