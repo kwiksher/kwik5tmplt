7ed083d343dcd5681a5e108dfa3dd90e2e2c553e
Naoya Yamamoto <nyamamoto@neoscorp.jp>
2024-09-14 22:50:21 +0900

    Add simulator notebook

--- xeus/create_page.lua
+++ xeus/create_page.lua
@@ -1,7 +1,8 @@
 local lfs = require("lfs")
+local M = {}
 
-local book = "hub"
-local arr = {"cafe", "collection", "inventory", "members", "mission", "party", "quest", "recruit", "shop"}
+-- local book = "hub"
+-- local arr = {"cafe", "collection", "inventory", "members", "mission", "party", "quest", "recruit", "shop"}
 
 local function mkdir(path)
     lfs.mkdir(path)
@@ -16,83 +17,90 @@ end
 local dst = lfs.currentdir()
 local tmp = ""
 
-mkdir(dst .. "/App/" .. book)
-lfs.chdir(dst .. "/App/" .. book)
-
-for _, page in ipairs(arr) do
-    tmp = tmp .. "'" .. page .. "', "
-    print(page)
-    mkdir("assets/images/" .. page)
-    mkdir("commands/" .. page)
-    mkdir("components/" .. page)
-    mkdir("components/" .. page .. "/audios")
-    mkdir("components/" .. page .. "/audios/long")
-    mkdir("components/" .. page .. "/audios/short")
-    mkdir("components/" .. page .. "/audios/sync")
-    mkdir("components/" .. page .. "/groups")
-    mkdir("components/" .. page .. "/layers")
-    mkdir("components/" .. page .. "/page")
-    mkdir("components/" .. page .. "/timers")
-    mkdir("components/" .. page .. "/variables")
-    mkdir("components/" .. page .. "/joints")
-    mkdir("models/" .. page)
-
-    local bgLuaContent = [[
-local M = {}
---
-function M:init(UI)
-end
---
-function M:create(UI)
-  local sceneGroup = UI.sceneGroup
-end
---
-function M:didShow(UI)
-end
---
-function M:didHide(UI)
-end
---
-function M:destroy(UI)
-end
---
-return M
-]]
-    writeFile("components/" .. page .. "/layers/bg.lua", bgLuaContent)
-
-    local indexLuaContent = [[
-local sceneName = ...
---
-local scene = require('controller.scene').new(sceneName, {
-    components = {
-      layers = { },
-      audios = { },
-      groups = { },
-      timers = { },
-      variables = { },
-      page = { }
-    },
-    commands = { },
-    onInit = function(scene) print("onInit") end
-})
---
-return scene
-]]
-    lfs.chdir("components/" .. page)
-    writeFile("index.lua", indexLuaContent)
-    lfs.chdir("../..")
-end
-
-print(tmp)
-
-lfs.chdir(dst .. "/App/" .. book)
+local bgLuaContent = [[
+  local M = {}
+  --
+  function M:init(UI)
+  end
+  --
+  function M:create(UI)
+    local sceneGroup = UI.sceneGroup
+  end
+  --
+  function M:didShow(UI)
+  end
+  --
+  function M:didHide(UI)
+  end
+  --
+  function M:destroy(UI)
+  end
+  --
+  return M
+  ]]
 
 local indexLuaContent = [[
-local scenes = {
-]] .. tmp .. [[
-}
-return scenes
-]]
-writeFile("index.lua", indexLuaContent)
+  local sceneName = ...
+  --
+  local scene = require('controller.scene').new(sceneName, {
+      components = {
+        layers = { {bg={}} },
+        audios = { },
+        groups = { },
+        timers = { },
+        variables = { },
+        page = { }
+      },
+      commands = { },
+      onInit = function(scene) print("onInit") end
+  })
+  --
+  return scene
+  ]]
 
-lfs.chdir(dst)
+
+function M.run(book, arr)
+
+  mkdir(dst .. "../Solar2D/App/" .. book)
+  lfs.chdir(dst .. "../Solar2D/App/" .. book)
+
+  for _, page in ipairs(arr) do
+      tmp = tmp .. "'" .. page .. "', "
+      print(page)
+      mkdir("assets/images/" .. page)
+      mkdir("commands/" .. page)
+      mkdir("components/" .. page)
+      mkdir("components/" .. page .. "/audios")
+      mkdir("components/" .. page .. "/audios/long")
+      mkdir("components/" .. page .. "/audios/short")
+      mkdir("components/" .. page .. "/audios/sync")
+      mkdir("components/" .. page .. "/groups")
+      mkdir("components/" .. page .. "/layers")
+      mkdir("components/" .. page .. "/page")
+      mkdir("components/" .. page .. "/timers")
+      mkdir("components/" .. page .. "/variables")
+      mkdir("components/" .. page .. "/joints")
+      mkdir("models/" .. page)
+
+      writeFile("components/" .. page .. "/layers/bg.lua", bgLuaContent)
+
+      lfs.chdir("components/" .. page)
+      writeFile("index.lua", indexLuaContent)
+      lfs.chdir("../..")
+  end
+
+  print(tmp)
+
+  lfs.chdir(dst .. "../Solar2D/App/" .. book)
+
+  local indexLuaContent = [[
+  local scenes = {
+  ]] .. tmp .. [[
+  }
+  return scenes
+  ]]
+  writeFile("index.lua", indexLuaContent)
+
+  lfs.chdir(dst)
+
+end
