c20e263f934d467b4f1e64e74a967cc66b118395
Naoya Yamamoto <yamamoto@kwiksher.com>
2024-10-01 12:38:32 +0900

    FIX rename audio

--- Solar2D/editor/controller/rename.lua
+++ Solar2D/editor/controller/rename.lua
@@ -6,32 +6,35 @@ local confirmation = require("editor.picker.confirmation")
 local scripts = require("editor.scripts.commands")
 --
 local function getListener(props, selections)
-  if selections and  #selections > 1 then
+  if selections and #selections > 1 then
     return nil, nil
   elseif props then
     if props.book then
-      print("rename book" )
+      print("rename book")
       return props.book, "book", scripts.renameBook
     elseif props.page then
       print("rename page", props.page)
       return props.page, "page", scripts.renamePage
     elseif props.audio then
       print("rename audio", props.audio, props.type)
-      return props.audio, "audio/"..props.type, scripts.renameAudio
+      return props.audio, "audio/" .. props.type, scripts.renameAudio
     elseif props.class == "group" then
       return props.group, "group", scripts.renameGroup
     elseif props.class == "timer" then
       return props.timer, "timer", scripts.renameTimer
     elseif props.class == "joint" then
-      return props.joint,  "joint", scripts.renameJoint
+      return props.joint, "joint", scripts.renameJoint
     elseif props.class == "variable" then
       return props.variable, "variable", scripts.renameVariable
+    else
+      print(props.class, selections[1].text)
+      -- print(props.class, selections[1].text, selections[1][props.class])
     end
   else
     print("rename layer")
-    return  props.layer, "layer", scripts.renameLayer
+    return props.layer, "layer", scripts.renameLayer
   end
-return
+  return
 end
 --
 local instance =
@@ -39,22 +42,28 @@ local instance =
   function(params)
     local UI = params.UI
     --
-    local oldValue,_type, script = getListener(params.props, UI.editor.selections)
+    local oldValue, _type, script = getListener(params.props, UI.editor.selections)
     --
     if _type then
       local function listener(value)
-         print("newValue=", value)
+        print("newValue=", value)
         if value and value == "Continue" and picker.obj.field.text:len() > 0 then
-          script(UI.book, UI.page, oldValue, picker.obj.field.text) -- _dst == Solar2D
+          if params.props.audio then
+            script(UI.book, UI.page, params.props.type, oldValue, picker.obj.field.text) -- _dst == Solar2D
+          else
+            script(UI.book, UI.page, oldValue, picker.obj.field.text) -- _dst == Solar2D
+          end
         else
           print("user cancel")
         end
         confirmation:destroy()
         picker:destroy()
       end
-      confirmation:create(listener, "Press Continue to rename "..oldValue, nil)
-      picker:create(nil,  "Please input a ".. _type.." name")
+      confirmation:create(listener, "Press Continue to rename " .. oldValue, nil)
+      picker:create(nil, "Please input a " .. _type .. " name")
       picker.obj.text = ""
+    else
+      print("error no type")
     end
   end
 )
