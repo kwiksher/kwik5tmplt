f1f6376365e12e9e033cdfb86b442ea176227082
Naoya Yamamoto <yamamoto@kwiksher.com>
2024-11-27 18:01:34 +0900

    FIX table position for activeProp

--- Solar2D/editor/group/groupTable.lua
+++ Solar2D/editor/group/groupTable.lua
@@ -193,7 +193,8 @@ function M:create(UI)
         self:hide()
       end
       if fooValue.isActiveProp then
-        self.group.x = display.contentCenterX+140
+        self.group.x = display.contentCenterX+120
+        self.group.y = display.contentCenterY-120
       end
 
     end
