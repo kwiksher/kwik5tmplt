40eb4472688e06c71feca52f1ebc7617c095167d
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-01-01 11:37:03 +0900

    FIX joints distance

--- Solar2D/template/components/pageX/physics/joint.lua
+++ Solar2D/template/components/pageX/physics/joint.lua
@@ -67,6 +67,24 @@ function M:create(UI)
   {{/gear}}
   }
 {{/properties}}
+  self:_create(UI)
+
+  --[[
+    local circleA = display.newCircle(self.properties.anchorA_x, self.properties.anchorA_y, 10)
+    circleA:setFillColor(1, 0, 0)
+    UI.sceneGroup:insert(circleA)
+
+    local B = display.newCircle(self.properties.anchorB_x, self.properties.anchorB_y, 10)
+    B:setFillColor(1, 1, 0)
+    UI.sceneGroup:insert(B)
+
+    local circle = display.newCircle(self.properties.anchor_x, self.properties.anchor_y, 10)
+    circle:setFillColor(1, 0, 0)
+    UI.sceneGroup:insert(circle)
+
+  --]]
+
+
 end
 
 return require("components.kwik.page_physicsJoint").set(M)
