8f283f30889e72474eace6076e12c19c52d4bc10
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-01-04 13:22:49 +0900

    FIX with getCenter

--- Solar2D/components/kwik/layer_image.lua
+++ Solar2D/components/kwik/layer_image.lua
@@ -6,7 +6,10 @@ local util = require "lib.util"
 function M:setProps(layerProps)
   self.imageWidth  = layerProps.width/4
   self.imageHeight = layerProps.height/4
-  self.mX, self.mY   = app.getPosition(layerProps.x, layerProps.y, self.align)
+  -- self.mX, self.mY   = app.getPosition(layerProps.x, layerProps.y, self.align)
+  local editorWidth, editorHeight = display.contentWidth - 480, display.contentHeight -320
+  self.mX, self.mY   = layerProps.x*0.25 + editorWidth/2 , layerProps.y*0.25 + editorHeight/2
+
   layerProps.mX, layerProps.mY = self.mX, self.mY
   --
   self.randXStart  = app.getPosition(layerProps.randXStart)
--- Solar2D/template/components/pageX/shape/new_text.lua
+++ Solar2D/template/components/pageX/shape/new_text.lua
@@ -1,7 +1,8 @@
 -- $weight={{weight}}
 --
 local parent,root, M = newModule(...)
-
+local app = require "controller.Application"
+--
 local _layerProps = {
   name     = "{{name}}",
   x        = {{x}},
@@ -31,6 +32,7 @@ end
 function M:create(UI)
   local layerProps = self.layerProps or _layerProps
   self.layerProps = layerProps
+  layerProps.x, layerProps.y = app.getCenter(layerProps.x, layerProps.y)
 
   local  obj = display.newText(layerProps)
 
