d0bc51b739c7d12af914eba391e608959a4b3ece
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-01-04 22:34:22 +0900

    Update touch, weld, rope

--- Solar2D/App/physics/components/rope/index.lua
+++ Solar2D/App/physics/components/rope/index.lua
@@ -23,7 +23,7 @@ local model = {
     },
     timers = {  },
     variables = {  },
-    joints    = { },
+    joints    = {  "rect_0_rect_1_rope",  },
     page = { "physics",  }
   },
   commands = {  },
--- Solar2D/App/physics/components/touch/layers/rect_0.lua
+++ Solar2D/App/physics/components/touch/layers/rect_0.lua
@@ -7,10 +7,10 @@ local app = require "controller.Application"
 
 local _layerProps = {
   name     = "rect_0",
-  x        = -24,
+  x        = -100,
   y        = -21,
-  width    =  59,
-  height    =  54,
+  width    =  50,
+  height    =  50,
   xScale = 1,
   yScale = 1,
   anchorX = 0,
