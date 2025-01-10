9d67faf76065b940ccc7dce8a8e19033a57da51d
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-01-10 12:46:32 +0900

    Add book:physics page:basic

---
+++ Solar2D/App/physics/components/basic/layers/_body.lua
@@ -0,0 +1,22 @@
+local M = {
+  name = "",
+  class="body",
+  dataPath = NIL, -- physicsEdtior(CodeAndWeb)
+  dataShape ={}
+}
+function M:create(UI)
+  local  = UI.sceneGroup[""]
+  self.properties = {
+      bounce = 1,
+      density = 1,
+      friction = 1,
+      gravityScale = 1,
+      isFixedRotation = false,
+      isSensor = false,
+      radius = 0, -- 0 means use object width/2 if cirlce is selected
+      shape   ="rectangle", -- "circle", -- rectangle,  path
+      type = "", -- kinematic, static, dynamic
+  }
+  self:_create(UI)
+end
+return require("components.kwik.layer_physicsBody").set(M)
