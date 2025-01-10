d301258fcff05a144581ec799528510eb42a1814
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-01-10 22:17:29 +0900

    Add book:physics basic with collision and force

---
+++ Solar2D/App/physics/components/basic/groups/hitGroup.lua
@@ -0,0 +1,14 @@
+local props = {
+  name       = "hitGroup",
+  members   = {
+    "rect_0",
+  },
+  properties = {
+    alpha = 1,
+    xScale = 1,
+    yScale = 1,
+    rotation = 0,
+    isLuaTable = false
+  }
+}
+return require("components.kwik.page_group").set(props)
---
+++ Solar2D/App/physics/components/basic/layers/ellipse_0_collision.lua
@@ -0,0 +1,14 @@
+local name = ...
+local parent, root = newModule(name)
+local M = {
+  name = "ellipse_0",
+  class="collision",
+  properties = {
+    body          = "ellipse_0",
+    isRemoveOther = true,
+    isRemoveSelf = true,
+    others  =  "hitGroup"
+  },
+  actions = { onCollision="" },
+}
+return require("components.kwik.layer_physicsCollision").set(M)
