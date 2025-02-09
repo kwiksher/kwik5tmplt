473eff19fb3f693b409e5cca407c7ccda3e1a1c6
Naoya Yamamoto <nyamamoto@neoscorp.jp>
2025-02-09 21:55:04 +0900

    Add book: interaction shape, spin, swipe

--- Solar2D/App/interaction/components/shake/index.lua
+++ Solar2D/App/interaction/components/shake/index.lua
@@ -9,11 +9,12 @@ local model = {
         }
       },
       {
-        rect_0 = {  }
+        rect_0 = {
+          class={ "shake", }  }
       },
       {
         ellipse_0 = {
-            }
+        }
       },
     },
     audios = {
@@ -25,7 +26,7 @@ local model = {
     joints    = {  },
     page = {  }
   },
-  commands = {  },
+  commands = {   "shakeAction",  },
   onInit = function(scene) print("onInit") end
 }
 local scene = require('controller.scene').new(sceneName, model)
