6ad51956fddbb7b83359239b254dd16ff040f3bb
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-02-11 15:48:14 +0900

    WIP unit testing for replacement

--- Solar2D/template/components/pageX/replacement/defaults/map.lua
+++ Solar2D/template/components/pageX/replacement/defaults/map.lua
@@ -3,10 +3,19 @@ local M = {
   class="map",
   properties = {
     target = NIL,
+    mapType         = "Normal", -- Normal, Satellite, Hybrid
+    latituite       = 37.331692,
+    longtitude      = -122.030456,
+    isScrollEnabled = true,
+    isZoomEnabled   = true,
+    marker = {
+      enabled    = true,
+      latituite  = 37.331692,
+      longtitude = -122.030456,
+      title      = "title",
+      subtitle   = "subtitle",
+    },
   },
-  actions = {
-    actionName = "",
-  }
 }
 
 return M
--- Solar2D/template/components/pageX/replacement/layer_map.lua
+++ Solar2D/template/components/pageX/replacement/layer_map.lua
@@ -1,10 +1,23 @@
-local parent,root, M = newModule(...)
+local parent, root, M = newModule(...)
 local layerProps = require(M.layerMod).layerProps
 
 local M = {
   name = "{{name}}",
   properties = {
     {{#properties}}
+    target          = "{{target}}","
+    mapType         = "{{mapType}}",
+    latituite       = "{{latituite}}",
+    longtitude      = "{{longtitude}}",
+    isScrollEnabled = {{isScrollEnabled}},
+    isZoomEnabled   = {{isZoomEnabled}},
+    marker = {
+      enabled    = {{marker.enabled}},
+      latituite  = "{{marker.latituite}}",
+      longtitude = "{{marker.longtitude}}",
+      title      = "{{marker.title}}",
+      subtitle   = "{{marker.subtitle}}",
+    },
     {{/properties}}
   },
   layerProps = layerProps
