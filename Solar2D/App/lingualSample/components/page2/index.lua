21db275d33a9f24bc2acccf81768d37aaac56fe9
Naoya Yamamoto <yamamoto@kwiksher.com>
2025-02-27 19:10:31 +0900

    Update lingualSample with text contents

--- Solar2D/App/lingualSample/components/page2/index.lua
+++ Solar2D/App/lingualSample/components/page2/index.lua
@@ -1,106 +1,148 @@
 local sceneName = ...
 --
 local model = {
-  --name = "",
-  components = {
-    layers = {
-      {
-        kwkcover = {
-        }
-      },
-      {
-        witch = {     {
-          en = {
-            class={ "linear", }  }
-        },
-        {
-          ja = {
-          }
-        },
-        {
-          pt = {
-          }
-        },
-        {
-          sp = {
-          }
-        },
-
-        class={ "lang", }  }
-      },
-      {
-        flyOver = {     {
-          en = {
-          }
-        },
-        {
-          ja = {
-          }
-        },
-        {
-          pt = {
-          }
-        },
-        {
-          sp = {
-          }
-        },
-
-        class={ "lang", }  }
-      },
-      {
-        fly = {     {
-          en = {
-            class={ "button", }  }
-        },
-        {
-          ja = {
-          }
-        },
-        {
-          pt = {
-          }
-        },
-        {
-          sp = {
-          }
-        },
-
-        class={ "lang", }  }
-      },
-      {
-        father = {     {
-          en = {
-            class={ "sync", }  }
-        },
-        {
-          ja = {
-          }
-        },
-        {
-          pt = {
-          }
-        },
-        {
-          sp = {
-          }
-        },
-
-        class={ "lang", }  }
-      },
+  --name = "page2",
+	components = {
+		layers = {
+			{
+				kwkcover = {
+					class = {  }
+				}
+			},
+			{
+				witch = {
+					{
+						pt = {
+							class = {  }
+						}
+					},
+					{
+						ja = {
+							class = {  }
+						}
+					},
+					{
+						sp = {
+							class = {  }
+						}
+					},
+					{
+						en = {
+							class = {
+								"linear",
+							}
+						}
+					},
+					class = {
+						"lang",
+					}
+				}
+			},
+			{
+				fly = {
+					{
+						en = {
+							class = {
+								"button",
+							}
+						}
+					},
+					{
+						sp = {
+							class = {  }
+						}
+					},
+					{
+						pt = {
+							class = {  }
+						}
+					},
+					{
+						ja = {
+							class = {  }
+						}
+					},
+					class = {
+						"lang",
+					}
+				}
+			},
+			{
+				flyOver = {
+					{
+						en = {
+							class = {  }
+						}
+					},
+					{
+						sp = {
+							class = {  }
+						}
+					},
+					{
+						pt = {
+							class = {  }
+						}
+					},
+					{
+						ja = {
+							class = {  }
+						}
+					},
+					class = {
+						"lang",
+					}
+				}
+			},
+			{
+				father = {
+					{
+						en = {
+							class = {
+								"sync",
+							}
+						}
+					},
+					{
+						sp = {
+							class = {  }
+						}
+					},
+					{
+						pt = {
+							class = {  }
+						}
+					},
+					{
+						ja = {
+							class = {  }
+						}
+					},
+					class = {
+						"lang",
+					}
+				}
+			},
+		},
+		audios = {
+			long = {  },
+			short = {   }
+		},
+		groups = {
     },
-    audios = {
-    },
-    groups = {
-    },
-    timers = {  },
-    variables = {  },
-    joints    = {  },
-    page = {  }
-  },
-  commands = {   "flyAnim",  },
-  onInit = function(scene) print("onInit") end
+		timers = {  },
+		variables = {  },
+		joints = {  },
+		page = {  }
+	},
+	commands = {
+		"flyAnim",
+	},
+	onInit = function(scene)
+		print("onInit")
+	end
 }
+
 local scene = require('controller.scene').new(sceneName, model)
 --
 return scene
