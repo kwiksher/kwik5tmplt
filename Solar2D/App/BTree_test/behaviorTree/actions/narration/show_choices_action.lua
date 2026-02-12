b72ad1a886008312c259709c59a97b2268159d2c
Naoya Yamamoto <yamamoto@kwiksher.com>
2026-02-12 17:30:27 +0900

    Delete

--- Solar2D/App/BTree_test/behaviorTree/actions/narration/show_choices_action.lua
+++
@@ -1,9 +0,0 @@
--- Show Choices Action Module
--- Displays the player choice buttons (Reload, Continue, etc.)
--- Returns RUNNING until user selects a choice AND the choice action completes
--- Uses BaseShowChoicesAction for common functionality
-
-local BaseShowChoicesAction = require("actions.base_show_choices_action")
-
--- Create module using base class
-return BaseShowChoicesAction.new()
--- Solar2D/App/BTree_test/behaviorTree/actions/narration/wait_action.lua
+++
@@ -1,8 +0,0 @@
--- Wait Action
--- Returns RUNNING to pause tree execution until clearWait is called
--- Uses BaseWaitAction for common functionality
-
-local BaseWaitAction = require("actions.base_wait_action")
-
--- Create module using base class
-return BaseWaitAction.new()
