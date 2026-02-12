1e3d504e66976bee95db440374e0967dbd1aac78
Naoya Yamamoto <yamamoto@kwiksher.com>
2026-02-12 17:57:17 +0900

    Delete

--- Solar2D/App/TheLastSpark/behaviorTree/actions/cabin/show_choices_action.lua
+++
@@ -1,8 +0,0 @@
--- Show Choices Action (Cabin Scene)
--- Uses base show choices action
-
-local BaseShowChoicesAction = require("actions.base_show_choices_action")
-
--- Create module using base class
-return BaseShowChoicesAction.new()
-
