9257fd3a7ce4a70e50683b552ceed983063e34f3
Kwiksher <yamamoto@kwiksher.com>
2026-02-15 12:10:28 +0900

    Delete

--- Solar2D/Behavior/BTree_test/views/_display_base.lua
+++
@@ -1,6 +0,0 @@
--------------------------------------------------------------------------------
--- Display Base Module
--- BTree_test wrapper for shared display base implementation
--------------------------------------------------------------------------------
-
-return require("Behavior.display_base")
--- Solar2D/Behavior/TheLastSpark/views/_display_base.lua
+++
@@ -1,6 +0,0 @@
--------------------------------------------------------------------------------
--- Display Base Module
--- TheLastSpark wrapper for shared display base implementation
--------------------------------------------------------------------------------
-
-return require("Behavior.display_base")
