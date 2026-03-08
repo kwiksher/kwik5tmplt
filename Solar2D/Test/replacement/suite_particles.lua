c1346518391eca03e060184a60c7a1c3767e5270
Kwiksher <yamamoto@kwiksher.com>
2026-03-08 22:30:44 +0900

    add test suite particles

--- Solar2D/Test/replacement/suite_particles.lua
+++ Solar2D/Test/replacement/suite_particles.lua
@@ -2,10 +2,18 @@ local M = require("Test.base_suite").new()
 
 local helper = require("Test.helper")
 
-function M.test_new()
+function M.xtest_new()
   local name = "rect_0"
   helper.selectLayer(name)
   helper.selectIcon("Replacements", "Particles"):done(function() end)
+end
+
+function M.test_select_particles()
+  local name = "rect_0"
+  M.layerTable.altDown = true
+  local obj = helper.selectLayer(name, "particles")
+  M.layerTable.altDown = false
+
 end
 
 return M
