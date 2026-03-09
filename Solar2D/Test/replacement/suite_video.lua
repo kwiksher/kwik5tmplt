4491e9f83ab56ee77b30316d113903903ec3e8fd
Kwiksher <yamamoto@kwiksher.com>
2026-03-09 10:13:19 +0900

    fix video

--- Solar2D/Test/replacement/suite_video.lua
+++ Solar2D/Test/replacement/suite_video.lua
@@ -2,10 +2,17 @@ local M = require("Test.base_suite").new()
 
 local helper = require("Test.helper")
 
-function M.test_new()
+function M.xtest_new()
   local name = "rect_0"
   helper.selectLayer(name)
   helper.selectIcon("Replacements", "Video"):done(function() end)
+end
+
+function M.test_select()
+  local name = "rect_0"
+  M.layerTable.altDown = true
+  helper.selectLayer(name, "video")
+  M.layerTable.altDown = false
 end
 
 return M
