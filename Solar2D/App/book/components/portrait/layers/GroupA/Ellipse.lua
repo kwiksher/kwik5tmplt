00ac609237343a7074f820337b7f08563f7927bb
Kwiksher <yamamoto@kwiksher.com>
2026-03-30 19:21:46 +0900

    Update book/portrait

--- Solar2D/App/book/components/portrait/layers/GroupA/Ellipse.lua
+++ Solar2D/App/book/components/portrait/layers/GroupA/Ellipse.lua
@@ -2,34 +2,50 @@
 --
 local app = require "controller.Application"
 local M = require("components.kwik.layer_image").new()
+local infinity = require("components.kwik.layer_image_infinity")
 
 local layerProps = {
   blendMode = "normal",
-  height    =  1600 - 1397,
-  width     = 1600 - 1400 ,
-  kind      = solidColor,
+  height    =  1920 - 1717,
+  width     = 1280 - 1080 ,
+  kind      = "solidColor",
   name      = "GroupA/Ellipse",
   type      = "png",
-  x         = 1600 + (1400 -1600)/2,
-  y         = 1397 + (1600 - 1397)/2,
+  x         = 1280 + (1080 -1280)/2,
+  y         = 1717 + (1920 - 1717)/2,
+  -- x         = bounds.right + bounds.left - bounds.right)/2,
+  -- y         = bounds.top + bounds.bottom - bounds.top)/2,
   alpha     = 100/100,
+  infinity = {
+  },
+  -- text properties
+  contents =  "",
+  font =  "",
+  fontSize =  nil,
+  alignment =  "",
+  orientation = "",
+  psdPage     = "portrait"
 }
 
-M.align       = ""
+M.attachToEdge      = ""
 M.randXStart  = nil
 M.randXEnd    = nil
 M.randYStart  = nil
 M.randYEnd    = nil
 --
-M.scaleX     = nil
-M.scaleY     = nil
+M.xScale     = nil
+M.yScale     = nil
 M.rotation   = nil
 --
 M.layerAsBg     = nil
-M.isSharedAsset = nil
 --
 M:setProps(layerProps)
 --
+-- Set isSharedAsset = true, and then set a common module. See kwikTheCatCommon.lua
+--
+M.isSharedAsset = nil
+M.imagePath   = "portrait/GroupA/Ellipse.png"
+
 function M:init(UI)
   --local sceneGroup = UI.scene.view
 	if not self.isSharedAsset then
@@ -41,13 +57,25 @@ function M:create(UI)
 	if not self.isSharedAsset then
     self.imagePath = UI.page ..self.imageName
   end
-  UI.layers[#UI.layers] = self:createImage(UI)
+  local obj = self:createImage(UI)
+  UI.layers[#UI.layers] = obj
+  self.obj = obj
+
+  if self.infinity and self.infinity.enabled then
+    infinity.createInfinityImage(UI, self.obj, self.infinity)
+  end
 end
 --
 function M:didShow(UI)
+  if self.infinity and self.infinity.enabled then
+    infinity.addEventListener(self.obj)
+  end
 end
 --
 function M:didHide(UI)
+  if self.infinity and self.infinity.enabled then
+    infinity.removeEventListener(self.obj)
+  end
 end
 --
 function  M:destroy(UI)
--- Solar2D/App/book/portrait.lua
+++ Solar2D/App/book/portrait.lua
@@ -1,63 +1,88 @@
 local sceneName = ...
 --
 local model = {
-  --name = "",
-  components = {
-    layers = {
-      {
-        bg = {
-        }
-      },
-      {
-        copyright = {
-        }
-      },
-      {
-        GroupA = {     {
-          SubA = {     {
-            Triangle = {
-              class={ "linear", }  }
-          },
-
-          }
-        },
-        {
-          Ellipse = {
-            class={ "button", }  }
-        },
-
-        }
-      },
-      {
-        star = {
-        }
-      },
-      {
-        tree = {
-        }
-      },
-      {
-        hello = {
-          class={ "sprite", }  }
-      },
-    },
-    audios = {
-      long={  }, short={ "Thunder",   }
-    },
-    groups = {
-      {
-        group0 = {
-          class={ "linear","button", }  }
-      },
-    },
-    timers = {  "timer1",  },
-    variables = {  },
-    joints    = {  },
-    page = {  }
-  },
-  commands = {   "audioAction",   "playAnim",   "testAction",  },
-  onInit = function(scene) print("onInit") end
+  --name = "portrait",
+	components = {
+		layers = {
+			{
+				bg = {
+					class = {  }
+				}
+			},
+			{
+				copyright = {
+					class = {  }
+				}
+			},
+			{
+				GroupA = {
+					{
+						SubA = {
+							{
+								Triangle = {
+									class = {
+										"linear",
+									}
+								}
+							},
+							class = {  }
+						}
+					},
+					{
+						Ellipse = {
+							class = {
+								"button",
+							}
+						}
+					},
+					class = {  }
+				}
+			},
+			{
+				star = {
+					class = {  }
+				}
+			},
+			{
+				hello = {
+					class = {
+						"sprite",
+					}
+				}
+			},
+		},
+		audios = {
+			long = {  },
+			short = {
+				"Thunder",
+			}
+		},
+		groups = {
+			{
+				group0 = {
+					class = {
+						"linear",
+						"button",
+					}
+				}
+			},
+		},
+		timers = {
+			"timer1",
+		},
+		variables = {  },
+		joints = {  },
+		page = {  }
+	},
+	commands = {
+		"audioAction",
+		"playAnim",
+		"testAction",
+	},
+	onInit = function(scene)
+	end
 }
+
 local scene = require('controller.scene').new(sceneName, model)
 --
 return scene
